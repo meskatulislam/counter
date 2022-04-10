9bab4e8648c4437822c2c92adf73e955393f3233
iammeskat <meskatcse@gmail.com>
2022-04-10 17:05:30 +0600

    update

--- src/components/app.component.jsx
+++ src/components/app.component.jsx
@@ -1,79 +1,85 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './navbar.component';
 import Reset from './reset.component';
 import Counters from './counters.component';
 
-class App extends Component {
-    state = {
-        counters: [
-            {id: 0, value: 0},
-            {id: 1, value: 5},
-            {id: 2, value: 9},
-            {id: 3, value: 1},
-            {id: 4, value: 0},
-        ]
-    };
+function App() {
+    const [counters, setCounters] = useState([
+        {id: 0, value: 0},
+        {id: 1, value: 5},
+        {id: 2, value: 9},
+        {id: 3, value: 1},
+        {id: 4, value: 0},
+    ]);
 
-    handleReset = () => {
-        const all_counters = this.state.counters.map((counter, index) => {
+    useEffect( () => {
+        console.log('=========>', counters);
+    });
+
+    const [name, setName] = useState('Pondit');
+
+    const updateName = () => {
+        setName('Meskat')
+    }
+    const handleReset = () => {
+        const all_counters = counters.map((counter, index) => {
             const obj = {id: index, value: 0};
             return obj;
         });
-        this.setState({counters: all_counters});
+        setCounters(all_counters);
         
     }
 
-    handleIncrement = (id) => {
-        const counters = this.state.counters.map((counter, idx) => {
+    const handleIncrement = (id) => {
+        const updated_counters = counters.map((counter, idx) => {
             if(counter.id === id) return { id: idx, value: counter.value+1};
             return counter;
         });
-        this.setState({counters: counters})
+        setCounters(updated_counters)
     }
 
-    handleDecrement = (id) => {
-        const counters = this.state.counters.map((counter, idx) => {
+    const handleDecrement = (id) => {
+        const updated_counters = counters.map((counter, idx) => {
             if(counter.value !== 0){
                 if(counter.id === id) return { id: idx, value: counter.value-1};
             }
             return counter;
         });
-        this.setState({counters: counters})
+        setCounters(updated_counters)
     }
 
-    getNonZeroItems = () => {
+    const getNonZeroItems = () => {
         let count = 0;
-        this.state.counters.forEach(counter => {
+        counters.forEach(counter => {
             if(counter.value > 0) count++;
         })
         return count;
     }
 
-    handleDelete = (id) => {
-        const counters = this.state.counters.filter(counter => counter.id !== id);
-        this.setState({counters: counters})
+    const handleDelete = (id) => {
+        const updated_counters = counters.filter(counter => counter.id !== id);
+        setCounters(updated_counters)
     }
 
-    render() {
-        return (
-            <>
-                <Navbar
-                    nonZeroItems={this.getNonZeroItems()}
-                />
+    return (
+        <>
+            <Navbar
+                nonZeroItems={getNonZeroItems()}
+            />
 
-                <Reset
-                    onReset={this.handleReset}
-                />
+            <Reset
+                onReset={handleReset}
+            />
 
-                <Counters 
-                    counters={this.state.counters}
-                    onIncrement={this.handleIncrement}
-                    onDecrement={this.handleDecrement}
-                    onDelete={this.handleDelete}
-                />
-            </>
-        );
-    }
+            <Counters 
+                counters={counters}
+                onIncrement={handleIncrement}
+                onDecrement={handleDecrement}
+                onDelete={handleDelete}
+            />
+
+        </>
+    );
 }
 
 export default App;
